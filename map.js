a3161b54e8e8b822f37deab370433e5c6df3ac2b
Aryslanbek Vakilov <119982220+avakilov@users.noreply.github.com>
2025-11-14 16:16:45 -0800

    Update map.js

--- map.js
+++ map.js
@@ -30,9 +30,8 @@ map.on('load', async () => {
     type: 'line',
     source: 'boston_route',
     paint: {
-      'line-color': 'green',
-      'line-width': 3,
-      'line-opacity': 0.4,
-    },
+        'line-color': '#32D400',  // A bright green using hex code
+        'line-width': 5,          // Thicker lines
+        'line-opacity': 0.6       // Slightly less transparent
+      },
   });
-  
